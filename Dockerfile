7030e0bf25f192e55709f9337a124831f0c2922e
Andra246 <andramayandra752@gmail.com>
2026-04-15 15:23:51 +0700

    m

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,10 @@ WORKDIR /app
 
 COPY app.js .
 
+ENV PORT=3000
+
+EXPOSE 3000
+
+HEALTHCHECK CMD wget --spider -q http://localhost:3000/health || exit 1
+
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req,res) =>{
 
 	if (req.url === '/health'){
-		res.writeHead(200);
-		res.end('OK');
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({status: 'OK'}));
 	}
 	else {
 		res.writeHead(404);
@@ -12,4 +14,6 @@ const server = http.createServer((req,res) =>{
 	}
 });
 
-server.listen(3000);
+server.listen(PORT, () => {
+	console.log(`port ${PORT}`)
+});
